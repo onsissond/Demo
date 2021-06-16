bebd9c7543e78b8ef67aca099f78f95f3833fc1b
Evgeny <sukhanovevgeniy96@gmail.com>
2021-06-16 12:12:34 +0300

    Added AppDelegate

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,14 @@
+// 
+// Copyright © 2021 onsissond. All rights reserved.
+//
+
+import SwiftUI
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        return true
+    }
+}
