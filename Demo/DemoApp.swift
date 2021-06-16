aca6cdd45e9ef15fd9ee0948a836f56435c57138
Evgeny <sukhanovevgeniy96@gmail.com>
2021-06-16 10:08:25 +0300

    Created project
    Added header config

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Евгений Суханов on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
